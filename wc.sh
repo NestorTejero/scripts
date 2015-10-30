40ff2ff2a7ef3aa822556087002c7ab96abe66d3
Nestor Tejero <tejero@textkernel.nl>
2015-10-30 14:17:25 +0100

    Simplify file giving, remove unused "case" statement for now

--- wc.sh
+++ wc.sh
@@ -5,36 +5,10 @@
 
 # alias wc="/path/to/wc.sh"
 
-
-lines=0
-chars=0
-bytes=0
-
-files=()
-while test $# -gt 0; do
-    case "$1" in
-        -l)
-            lines=1
-            shift
-            ;;
-        -c)
-            chars=1
-            shift
-            ;;
-        -b)
-            bytes=1
-            shift
-            ;;
-        *)
-            files+=("$1")
-            shift
-            ;;
-    esac
-done
+files=( "$@" )
 
 wc "${files[@]}" | \
-awk -v lines="$lines" -v chars="$chars" -v bytes="$bytes" \
-    'BEGIN{OFS="\t";names[0]="lines"; names[1]="words"; names[2]="bytes"}
+awk 'BEGIN{OFS="\t";names[0]="lines"; names[1]="words"; names[2]="bytes"}
      {filename=gensub(/([^ ]+ +){3}/,"", "g", $0);
      printf "<< %s >>\n", filename;
      for (i=1;i<=3;i++) {
